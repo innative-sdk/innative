e947abc7cb87b467cccbae95570b49c030e3bccb
Elliott.Wen <elliott-wen@users.noreply.github.com>
2019-11-19 13:01:41 +1300

    Update build-llvm.sh

--- build-llvm.sh
+++ build-llvm.sh
@@ -23,12 +23,11 @@ else
   cmake ../../llvm-project/llvm -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE:STRING="MinSizeRel" -DLLVM_TARGETS_TO_BUILD:STRING="X86;WebAssembly" -DLLVM_BUILD_LLVM_DYLIB:BOOL=OFF -DLLVM_ENABLE_TERMINFO:BOOL=OFF -DLLVM_INCLUDE_DOCS:BOOL=OFF -DLLVM_OPTIMIZED_TABLEGEN:BOOL=ON -DLLVM_INCLUDE_EXAMPLES:BOOL=OFF -DLLVM_INCLUDE_TESTS:BOOL=OFF -DLLVM_INCLUDE_BENCHMARKS:BOOL=OFF -DLLVM_ENABLE_ZLIB:BOOL=OFF -DLLVM_APPEND_VC_REV:BOOL=OFF
   if [$? -eq 0]
   then
-      ninja
+      make -j $CORES
   else
       echo "CMake failed, please check whether CMake is available in your system"
       exit -1
   fi
-  make -j $CORES
 fi
 cd ../..
 
