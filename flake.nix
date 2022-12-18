ea0a2ff12d7199b21bd05ac505250a4bf9c307bb
Erik McClure <erikm@fundament.software>
2022-12-17 17:38:53 -0800

    Cleanup and refactoring

--- flake.nix
+++ flake.nix
@@ -11,50 +11,54 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-    in {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-          selfpkgs = self.packages.${system};
-          llvmpkgs = pkgs.llvmPackages_13.tools.extend (_: _: { enableSharedLibraries = false; });
-        in {
-          innative = pkgs.stdenv.mkDerivation {
-            pname = "innative";
-            version = "0.2.0";
-            src = nix-filter.lib.filter {
-              root = ./.;
-              include = [
-                "CMakeLists.txt"
-                (nix-filter.lib.inDirectory "innative")
-                (nix-filter.lib.inDirectory "innative-assemblyscript")
-                (nix-filter.lib.inDirectory "innative-cmd")
-                (nix-filter.lib.inDirectory "innative-env")
-                (nix-filter.lib.inDirectory "innative-loader")
-                (nix-filter.lib.inDirectory "innative-stub")
-                (nix-filter.lib.inDirectory "innative-test")
-                (nix-filter.lib.inDirectory "innative-test-embedding")
-                (nix-filter.lib.inDirectory "include")
-                (nix-filter.lib.inDirectory "scripts")
-                (nix-filter.lib.inDirectory "spec")
+    in
+    {
+      packages = forAllSystems
+        (system:
+          let
+            pkgs = nixpkgsFor.${system};
+            selfpkgs = self.packages.${system};
+            llvmpkgs = pkgs.llvmPackages_13.tools.extend (_: _: { enableSharedLibraries = false; });
+          in
+          {
+            innative = pkgs.stdenv.mkDerivation {
+              pname = "innative";
+              version = "0.2.0";
+              dontStrip = true;
+              src = nix-filter.lib.filter {
+                root = ./.;
+                include = [
+                  "CMakeLists.txt"
+                  (nix-filter.lib.inDirectory "innative")
+                  (nix-filter.lib.inDirectory "innative-assemblyscript")
+                  (nix-filter.lib.inDirectory "innative-cmd")
+                  (nix-filter.lib.inDirectory "innative-env")
+                  (nix-filter.lib.inDirectory "innative-loader")
+                  (nix-filter.lib.inDirectory "innative-stub")
+                  (nix-filter.lib.inDirectory "innative-test")
+                  (nix-filter.lib.inDirectory "innative-test-embedding")
+                  (nix-filter.lib.inDirectory "include")
+                  (nix-filter.lib.inDirectory "scripts")
+                  (nix-filter.lib.inDirectory "spec")
+                ];
+              };
+              enableParallelBuilding = true;
+
+              nativeBuildInputs = [ pkgs.cmake ];
+              buildInputs = [
+                llvmpkgs.libclang
+                llvmpkgs.llvm.dev
+                llvmpkgs.lld
+                pkgs.libxml2
               ];
+
+              cmakeFlags = [ "-DUSE_DEFAULT_FOLDERS=1" "-DUSE_BUILD_OVERRIDE=1" ];
+
+              outputs = [ "out" ];
             };
-            enableParallelBuilding = true;
+          }
+        );
 
-            nativeBuildInputs = [ pkgs.cmake ];
-            buildInputs = [
-              llvmpkgs.libclang
-              llvmpkgs.llvm.dev
-              llvmpkgs.lld
-              pkgs.libxml2
-            ];
-
-            cmakeFlags = [ "-DUSE_DEFAULT_FOLDERS=1" "-DUSE_BUILD_OVERRIDE=1"];
-
-            outputs = [ "out" ];
-          };
-        }
-      );
-      
       defaultPackage = forAllSystems (system: self.packages.${system}.innative);
       devShell = forAllSystems (system: self.packages.${system}.innative);
     };
